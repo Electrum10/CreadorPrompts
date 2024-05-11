5b85b67e1d0c218eab9645446d337c5c3893aa6c
Jan Gango <jangangolells@gmail.com>
2024-05-11 15:40:50 +0200

    corrigiendo ortografía

--- script.py
+++ script.py
@@ -16,21 +16,21 @@ ventana.configure(background = Fondo)
 #Función para el botón de características
 def Características():
     R = Cámara.get()
-    RR = f"Actua como un experto en fotografia. Dime la Resolució/ISO, el Sensor, els punts d'enfocament, el Estabilitzador, el Video, la Conectivitat, el Visor, las dimensiones i Peso, la Bateria i la Resistencia de la cámara " + R
+    RR = f"Actúa como un experto en fotografía. Dime la Resolución/ISO, el Sensor, los puntos d'enfoque, el estabilizador, el Video, la conectividad, el Visor, las dimensiones i Peso, la Batería (mAh) i la Resistencia de la cámara " + R
     print(RR)
     pyperclip.copy(RR)
 
 #Función para el botón de atributos
 def Atributos():
     R = Cámara.get()
-    RS = f"Actua como un experto en fotografia. Dime los siguientes atributos de la cámara " + R + ": Precio, Empuñadura, Facilidad de uso i portabilidad"
+    RS = f"Actúa como un experto en fotografía. Dime los siguientes atributos de la cámara " + R + ": Precio, Empuñadura, Facilidad de uso i portabilidad"
     print(RS)
     pyperclip.copy(RS)
 
 
 def ProsContras():
     R = Cámara.get()
-    RS = f"Actua como un experto en fotografia. Dime los pros y los contras de la cámara" + R + ", pero haz unos pros y contras que demuestren tanto lo bueno como lo malo de la cámara."
+    RS = f"Actúa como un experto en fotografía. Dime los pros y los contras de la cámara" + R + ", pero haz unos pros y contras que demuestren tanto lo bueno como lo malo de la cámara."
     print(RS)
     pyperclip.copy(RS)
 
@@ -46,20 +46,20 @@ Titulo = tk.Label(ventana, font=FuenteTitulo, text="Automatizador de prompts", b
 Titulo.place(x=70, y=30)
 
 #Contexto
-Contexto = tk.Label(ventana, text="Pequeño programa para accelerar mis prompts para mi pagina web", font = FuenteContexto, background=Fondo)
+Contexto = tk.Label(ventana, text="Pequeño programa para acelerar mis prompts para mi pagina web", font = FuenteContexto, background=Fondo)
 Contexto.place(x = 120, y = 83)
 
 #Entrada para poner la cámara
 Cámara = tk.Entry(ventana, width = 30, font = FuenteCámara)
 Cámara.place(x = 180, y = 150)
 
-#Titulo para saber donde poner la camara
+#Titulo para saber donde poner la Camara
 CámaraTitulo = tk.Label(ventana, font=FuenteTexto, text="Cámara:", background = Fondo)
 CámaraTitulo.place(x = 250, y = 115)
 
 #Botón para las características
-CatacteristicasBotón = tk.Button(ventana, text = "Caracteristicas", background = "blue", width = 20, height = 3, font= FuenteBotón, command= Características)
-CatacteristicasBotón.place(x = 150, y = 200)
+CaracterísticasBotón = tk.Button(ventana, text = "Características", background = "blue", width = 20, height = 3, font= FuenteBotón, command= Características)
+CaracterísticasBotón.place(x = 150, y = 200)
 
 AtributosBotón = tk.Button(ventana, text = "Atributos", background = "pink", width = 20, height = 3, font= FuenteBotón, command= Atributos)
 AtributosBotón.place(x = 150, y = 320)
